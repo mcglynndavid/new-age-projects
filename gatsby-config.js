f2e4be468bea88560bdab96ad3c69a1497510754
semgrep-svcaccount <product-security+gitlab@pingidentity.com>
2023-02-10 18:23:30 -0800

    updating bacground colour from gatsby default

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#FFC800`,
-        theme_color: `#FFC800`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/background.jpg`, // This path is relative to the root of the site.
       },
