18a121dee78000b8aa6882dd5f0902eeb75dec71
davidmcglynn <davidmcglynn@pingidentity.com>
2023-02-15 17:04:48 -0800

    icons fixes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,11 @@ import { SocialIcon, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, Socia
 import Map from "../../Backgrounds/locations.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FaEnvelope } from '@fortawesome/fontawesome-svg-core'
+import {
+	faInstagram,
+	faLinkedinIn,
+	faFacebookF,
+} from "@fortawesome/free-brands-svg-icons";
 
 function ContactForm() {
   return (
@@ -13,20 +18,36 @@ function ContactForm() {
       <h2>Get in touch today to see how we can help with your next project.</h2>
         <a href='mailto:info@newageprojects.ca' >info@newageprojects.ca</a>
         <a href='tel:+16044407039'>+1-604-440-7039</a>
-        <p2>Or get in touch via social media:</p2> 
-          <p2>
-          <SocialIcons>
-            <SocialIconLink href="https://www.linkedin.com/company/new-age-projects/" target="_blank" aria-label="Linkedin">
-              <FaLinkedin  size="3x"/>
-            </SocialIconLink>
-            <SocialIconLink href="https://www.facebook.com/newageprojects" target="_blank" aria-label="Facebook">
-              <FaFacebook size="3x"/>
-            </SocialIconLink>
-            <SocialIconLink href="https://www.instagram.com/newageprojectsca" target="_blank" aria-label="Instagram">
-              <FaInstagram  size="3x"/>
-            </SocialIconLink>
-          </SocialIcons>
-        </p2>
+        <p2>Or get in touch via social media:</p2>
+        <p2> 
+          <a
+            href="https://www.linkedin.com/company/new-age-projects/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button type="button" className="btn-- btn-l">
+              <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+            </button>
+          </a>
+          <a
+          href="https://www.facebook.com/newageprojects"
+          target="_blank"
+          rel="noreferrer"
+          >
+            <button type="button" className="btn-- btn-f">
+              <FontAwesomeIcon icon={faFacebookF} size="2x" />
+            </button>
+          </a>
+          <a 
+            href="https://www.instagram.com/newageprojectsca"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button type="button" className="btn-- btn-i ">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </button>
+          </a>
+      </p2>  
     </div>
   )
 }
